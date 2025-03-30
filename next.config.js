a29f1afc0aeee4452399dab598065e43da5be528
MonkeyMan <110273989+SumitDon1@users.noreply.github.com>
2025-03-30 15:07:11 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+      GOOGLE_CLIENT_ID: "772987801312-3rc06o23qd2iaepaiht5809c42fuvqtf.apps.googleusercontent.com",
+      GOOGLE_CLIENT_SECRET: "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",
+      MONGODB_URI: "mongodb+srv://Kumarsumit39699:<db_password>@bookingjini.ndsbvo2.mongodb.net/?retryWrites=true&w=majority&appName=bookingJini",
+      NEXTAUTH_URL: "http://localhost:3000",
+      NEXTAUTH_SECRET: "your-secret-key-here"
+    }
+  }
